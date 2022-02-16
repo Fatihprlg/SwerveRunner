2b89e313e24f15ee9437f6b97f630a8cb1053431
Mücahit Fatih Parlağı <m.fatihparlagi@gmail.com>
2022-02-16 21:41:47 +0300

    Finish camera effect added

--- Assets/Scripts/Components/Player/CameraMovement.cs
+++ Assets/Scripts/Components/Player/CameraMovement.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     [SerializeField] GameObject target;
+    [SerializeField] GameObject winCamPos;
     [SerializeField] Vector3 offset;
     [SerializeField] float camSpeed = 3;
 
     void LateUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * camSpeed);
+        if (GameController.Instance.isSuccess)
+        {
+            winCamPos.transform.parent = null;
+            transform.position = Vector3.Lerp(transform.position, winCamPos.transform.position, Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, winCamPos.transform.rotation, Time.deltaTime);
+        }
+        else
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + offset, Time.deltaTime * camSpeed);
     }
 }
